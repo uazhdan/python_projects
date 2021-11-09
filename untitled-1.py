540f2bec1c58fd75afc5f2403e2e3f1cc92e018a
Andrey <a.zdanov@gmail.com>
2021-11-09 06:41:53 +0300

    Add files

--- untitled-1.py
+++ untitled-1.py
@@ -1,13 +1,2 @@
 
-def only_char(text):
-    result = ''
-    for i in text:
-        if i.isalpha():
-            result += i
-    return result
-
-
-text_in = 'Day, mice. "Year" is a mistake!'.split()
-for word in text_in:
-    print(len([i for i in word if i.isalpha()]))
-    print(len(only_char(word)))
+print(1 * 16 **3 + 10 * 16 ** 2 + 15 * 16 + 2)
