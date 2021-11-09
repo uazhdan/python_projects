c4a2f8daee23bdd7e1cb908a1f880adcbc7c31ae
Andrey Zhdanov <A.Zdanov@gmail.com>
2021-11-09 06:17:20 +0300

    Update Шифр Цезаря v0.3.py
    Оптимизировал

--- Шифр Цезаря v0.3.py
+++ Шифр Цезаря v0.3.py
@@ -1,29 +1,15 @@
-# функция, которая оставляет только символы в строке
-def only_char(text):
-    result = ''
-    for i in text:
-        if i.isalpha():
-            result += i
-    return result
-
-
-# функция кодирования
+#  функция кодирования
 def code_text(text):
     text_code = []
     #  сгенерируем алфавит eng из таблицы юникода
     abc = ''.join([chr(i) for i in range(97, 97 + 26)])
     for word in text:
-        step = len(only_char(word))
+        step = len([i for i in word if i.isalpha()])
         for a in word:
             if a.isalpha():
                 flag_l = a == a.lower()  # запомним регистр буквы
                 index_abc = abc.find(a.lower())
-                len_abc = len(abc)
-
-                if index_abc + step < len_abc:  # буква + шаг не выходят за пределы последовательности
-                    a = abc[index_abc + step]
-                else:  # вышли за пределы алфавита, хвостик идет сначала
-                    a = abc[index_abc + step - len_abc]
+                a = abc[(index_abc + step) % 26]
 
                 if flag_l:
                     text_code.append(a)
@@ -35,9 +21,8 @@ def code_text(text):
     return text_code
 
 
-# ##########################################################
 # основной модуль программы
-# ввод настроект
+# ввод настроек
 text_in = input('Укажите строку текста: ').split()
 text_out = code_text(text_in)
 
